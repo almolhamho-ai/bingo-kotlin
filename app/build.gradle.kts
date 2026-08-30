7f1424518782d1eebf358ea69cc25dc670737ffd
almolhamho-ai <almolham.ho@gmail.com>
2026-08-30 15:53:35 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.almolham.bingo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 50
-        versionName = "0.50"
+        versionCode = 51
+        versionName = "0.51"
     }
 
     signingConfigs {
